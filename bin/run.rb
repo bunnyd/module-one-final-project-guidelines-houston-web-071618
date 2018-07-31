2cf75ceac2f721916801a064098995aff84c1a1d
github username <github email address>
2018-07-31 15:58:39 -0500

    updated gem file. updated run to include API calls

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,8 @@
 require_relative '../config/environment'
 
-
+students = JSON.parse(RestClient.get("https://sheetsu.com/apis/v1.0su/678accd3ba54"))
+instructors = JSON.parse(RestClient.get("https://sheetsu.com/apis/v1.0su/d404a192d9c2"))
+study_sessions = JSON.parse(RestClient.get("https://sheetsu.com/apis/v1.0su/7fbb179f0f52"))
+binding.pry
 
 puts "HELLO WORLD"
